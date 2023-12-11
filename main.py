63d6dd452e0fe477132943fbb51aab7812a14d9f
chase <leo.shatov@gmail.com>
2023-12-11 18:00:16 +0300

    Практическое задание 3

---
+++ main.py
@@ -0,0 +1,27 @@
+import random
+from collections import defaultdict
+
+text = open('che.txt', encoding='utf8').read()
+corpus = text.split()
+def make_triples(corpus):
+     for i in range(len(corpus)-2):
+         yield (corpus[i], corpus[i+1], corpus[i+2])
+
+triples = make_triples(corpus)
+def make_chain(triples):
+    chain = defaultdict(list)
+    for word_1, word_2, word_3 in triples:
+        chain[(word_1, word_2)].append(word_3)
+    return chain
+
+chain = make_chain(triples)
+def generate_text(chain, max_length=100):
+    word_1, word_2 = random.choice(list(chain.keys()))
+    new_text = [word_1, word_2]
+    while len(new_text) < max_length:
+        word_1, word_2 = word_2, random.choice(chain[(word_1, word_2)])
+        new_text.append(word_2)
+    return ' '.join(new_text)
+
+new_text = generate_text(chain)
+print(new_text)
